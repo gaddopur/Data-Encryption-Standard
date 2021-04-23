c13b622841f375abe4f182b7546b745e053ea234
Aman <amansingh.eee18@itbhu.ac.in>
2021-04-23 08:21:39 +0530

    completed

---
+++ des.py
@@ -0,0 +1,351 @@
+import random
+
+def generate_permutation(n:int):
+    if n == 64:
+        perm = [ 58, 50, 42, 34, 26, 18, 10, 2, 
+               60, 52, 44, 36, 28, 20, 12, 4, 
+               62, 54, 46, 38, 30, 22, 14, 6, 
+               64, 56, 48, 40, 32, 24, 16, 8, 
+               57, 49, 41, 33, 25, 17, 9, 1, 
+               59, 51, 43, 35, 27, 19, 11, 3, 
+               61, 53, 45, 37, 29, 21, 13, 5, 
+               63, 55, 47, 39, 31, 23, 15, 7 ]
+        upd(perm)
+        return perm
+
+    perm = [x for x in range(n)]
+    random.shuffle(perm)
+    return perm
+
+def generate_half_permutation(n:int):
+    if n == 32:
+        perm = [ 16, 7, 20, 21, 
+                 29, 12, 28, 17, 
+                 1, 15, 23, 26, 
+                 5, 18, 31, 10, 
+                 2, 8, 24, 14, 
+                 32, 27, 3, 9, 
+                 19, 13, 30, 6, 
+                 22, 11, 4, 25 ]
+        upd(perm)
+        return perm
+
+    perm = [x for x in range(n)]
+    random.shuffle(perm)
+    return perm
+
+def generate_inv_permutation(n:int, perm):
+    if n == 64:
+        inv_perm = [ 40, 8, 48, 16, 56, 24, 64, 32, 
+             39, 7, 47, 15, 55, 23, 63, 31, 
+             38, 6, 46, 14, 54, 22, 62, 30, 
+             37, 5, 45, 13, 53, 21, 61, 29, 
+             36, 4, 44, 12, 52, 20, 60, 28, 
+             35, 3, 43, 11, 51, 19, 59, 27, 
+             34, 2, 42, 10, 50, 18, 58, 26, 
+             33, 1, 41, 9, 49, 17, 57, 25 ]
+        upd(inv_perm)
+        return inv_perm
+
+    inv_perm = [x for x in range(n)]
+    for i in range(n):
+        inv_perm[perm[i]] = i
+    return inv_perm
+
+def generate_expansion_permutation(n:int, m:int): 
+    ## n denotes expansion permutation length
+    ## m denotes half of blocksize
+    if m == 32:
+        exp_perm = [ 32, 1, 2, 3, 4, 5,
+             4, 5, 6, 7, 8, 9, 
+             8, 9, 10, 11, 12, 13, 
+             12, 13, 14, 15, 16, 17, 
+             16, 17, 18, 19, 20, 21, 
+             20, 21, 22, 23, 24, 25,
+             24, 25, 26, 27, 28, 29,
+             28, 29, 30, 31, 32, 1 ] 
+        upd(exp_perm)
+        return exp_perm
+
+    exp_perm = [0]*n
+    perm = [i for i in range(m)]
+    random.shuffle(perm)
+    for i in range(n):
+        exp_perm[i] = perm[i%m]
+    random.shuffle(exp_perm)
+    return exp_perm
+
+def generate_pc1_permutation(n):
+    if n == 64:
+        PC1 = [ 57, 49, 41, 33, 25, 17, 9, 
+             1, 58, 50, 42, 34, 26, 18, 
+             10, 2, 59, 51, 43, 35, 27, 
+             19, 11, 3, 60, 52, 44, 36, 
+             63, 55, 47, 39, 31, 23, 15, 
+             7, 62, 54, 46, 38, 30, 22, 
+             14, 6, 61, 53, 45, 37, 29, 
+             21, 13, 5, 28, 20, 12, 4 ]
+        upd(PC1)
+        return PC1
+    x = [i for i in range(n)]
+    random.shuffle(x)
+    perm = []
+    for i in range(n):
+        if (i+1)%8 != 0:
+            perm.append(x[i])
+    return perm
+
+def generate_pc2_permutation(n):
+    if n == 56:
+        PC2 = [ 14, 17, 11, 24, 1, 5, 
+             3, 28, 15, 6, 21, 10, 
+             23, 19, 12, 4, 26, 8, 
+             16, 7, 27, 20, 13, 2, 
+             41, 52, 31, 37, 47, 55, 
+             30, 40, 51, 45, 33, 48, 
+             44, 49, 39, 56, 34, 53, 
+             46, 42, 50, 36, 29, 32 ]
+        upd(PC2)
+        return PC2
+
+    x = [i for i in range(n)]
+    random.shuffle(x)
+    perm = []
+    for i in range(n):
+        if (i+1)%7 != 0:
+            perm.append(x[i])
+    return perm
+
+def upd(perm):
+    for i in range(len(perm)):
+        perm[i] -= 1
+
+def permutaion(msg:str, perm):
+    per_msg = []
+    for i in range(len(perm)):
+        per_msg.append(msg[perm[i]])
+    return "".join(per_msg)
+
+def leftshift(msg, k):
+    return msg[k:] + msg[:k]
+
+def xor(msg1, msg2):
+    x = []
+    for i in range(len(msg1)):
+        if msg1[i] == msg2[i]:
+            x.append('0')
+        else: x.append('1')
+    return "".join(x)
+
+def S_box(msg, s):
+    new_msg = []
+    num = 0
+    assert len(msg)%6 == 0, print("length of message must be divisible by 6")
+    for i in range(0, len(msg), 6):
+        cur = msg[i:i+6]
+        r = int(cur[0]+cur[5], 2)
+        c = int(cur[1:5], 2)
+        new_msg.append(str(bin(s[num][r][c])[2:].rjust(4, '0')))
+        num += 1
+        num %= 8
+
+    return "".join(new_msg)
+
+def mutate(txt: str, hamming_dist: int) -> str:
+  length = len(txt)
+  random_pos = random.sample(range(0,length),hamming_dist)
+  random_pos.sort() # To ensure the for loop below doesnot increase the length of txt
+  mutated_txt = ""
+  i = 0
+  for pos in random_pos:
+    mutated_txt += txt[i:pos]
+    i = pos+1
+    if(txt[pos] == '1'):
+      mutated_txt += '0'
+    else:
+      mutated_txt += '1'
+  
+  mutated_txt += txt[i:]
+  return mutated_txt
+
+def preprocess_key(key: str, halfwidth:int =32, hamming_dist:int =0) -> str:
+ 
+  if(len(key) > 2*halfwidth):
+    key = key[:2*halfwidth]
+  key = key.zfill(2*halfwidth)
+
+  if(hamming_dist != 0):
+    key = mutate(key,hamming_dist)
+
+  return key
+
+def Weak_key_demonstration(key, num_rounds, block_size):
+    PC1 = generate_pc1_permutation(block_size)
+    PC2 = generate_pc2_permutation(len(PC1))
+    key = permutaion(key, PC1)
+    keys = []
+    L_key = key[:len(key)//2]
+    R_key = key[len(key)//2:]
+    for i in range(num_rounds):
+        L_key = leftshift(L_key, 1)
+        R_key = leftshift(R_key, 1)
+
+        cur_key = permutaion((L_key+R_key), PC2)
+        keys.append(cur_key)
+    return keys
+
+def encrypt(msg, perm, inv_perm, exp_perm, key, PC1, PC2, half_perm, s_boxes, num_rounds):
+
+    half = len(half_perm)
+
+    msg = permutaion(msg, perm)
+    L_msg = msg[0:half]
+    R_msg = msg[half:]
+
+    key = permutaion(key, PC1)
+    L_key = key[:len(key)//2]
+    R_key = key[len(key)//2:]
+
+    msg = []
+
+    for i in range(num_rounds):
+        newL_msg = R_msg
+        R_msg = permutaion(R_msg, exp_perm)
+
+        L_key = leftshift(L_key, 1)
+        R_key = leftshift(R_key, 1)
+
+        cur_key = permutaion((L_key+R_key), PC2)
+        assert len(cur_key) == len(R_msg), print("lenght of current key and R_msg should be equal")
+        R_msg = xor(cur_key, R_msg)
+        R_msg = S_box(R_msg, s_boxes)
+        R_msg = permutaion(R_msg, half_perm)
+        R_msg = xor(R_msg, L_msg)
+        L_msg = newL_msg
+        if i != num_rounds-1:
+            msg.append(L_msg+R_msg)
+
+    L_msg, R_msg = R_msg, L_msg
+    msg.append(permutaion(L_msg+R_msg, inv_perm))
+    return msg
+
+
+def txt_bin(s:str):
+    bin_msg = []
+    for c in s:
+        bin_msg.append(str(bin(ord(c)))[2:].rjust(8, '0'))
+    return "".join(bin_msg)
+
+def bin_txt(msg):
+    enc_msg = []
+    for i in range(0, len(msg), 8):
+        ascii_val = int(msg[i:i+8], 2)
+        ch = chr(ascii_val)
+        enc_msg.append(ch)
+    return "".join(enc_msg)
+
+def one_bit_diff(msg):
+    bin_msg = txt_bin(msg)
+    bin_msg = list(bin_msg)
+    if bin_msg[0] == '0':
+        bin_msg[0] = '1'
+    else: bin_msg[0] = '0'
+    bin_msg = "".join(bin_msg)
+    return bin_txt(bin_msg)
+
+def preprocess(s, half, block_size, num_rounds, key):
+
+    bin_msg = txt_bin(s)
+    mul = (len(bin_msg)+block_size-1)//block_size*block_size
+    bin_msg = bin_msg.rjust(mul, '0')
+
+    s_boxes = [
+       [ 
+         [ 14, 4, 13, 1, 2, 15, 11, 8, 3, 10, 6, 12, 5, 9, 0, 7], 
+         [0, 15, 7, 4, 14, 2, 13, 1, 10, 6, 12, 11, 9, 5, 3, 8], 
+         [4, 1, 14, 8, 13, 6, 2, 11, 15, 12, 9, 7, 3, 10, 5, 0], 
+         [15, 12, 8, 2, 4, 9, 1, 7, 5, 11, 3, 14, 10, 0, 6, 13]
+       ], 
+       [ 
+         [15, 1, 8, 14, 6, 11, 3, 4, 9, 7, 2, 13, 12, 0, 5, 10], 
+         [3, 13, 4, 7, 15, 2, 8, 14, 12, 0, 1, 10, 6, 9, 11, 5],
+         [0, 14, 7, 11, 10, 4, 13, 1, 5, 8, 12, 6, 9, 3, 2, 15],
+         [13, 8, 10, 1, 3, 15, 4, 2, 11, 6, 7, 12, 0, 5, 14, 9]
+       ], 
+       [ 
+         [10, 0, 9, 14, 6, 3, 15, 5, 1, 13, 12, 7, 11, 4, 2, 8],
+         [13, 7, 0, 9, 3, 4, 6, 10, 2, 8, 5, 14, 12, 11, 15, 1],
+         [13, 6, 4, 9, 8, 15, 3, 0, 11, 1, 2, 12, 5, 10, 14, 7],
+         [1, 10, 13, 0, 6, 9, 8, 7, 4, 15, 14, 3, 11, 5, 2, 12 ]
+       ],
+       [ 
+         [7, 13, 14, 3, 0, 6, 9, 10, 1, 2, 8, 5, 11, 12, 4, 15], 
+         [13, 8, 11, 5, 6, 15, 0, 3, 4, 7, 2, 12, 1, 10, 14, 9],
+         [10, 6, 9, 0, 12, 11, 7, 13, 15, 1, 3, 14, 5, 2, 8, 4], 
+         [3, 15, 0, 6, 10, 1, 13, 8, 9, 4, 5, 11, 12, 7, 2, 14 ]
+       ], 
+       [ 
+         [2, 12, 4, 1, 7, 10, 11, 6, 8, 5, 3, 15, 13, 0, 14, 9],
+         [14, 11, 2, 12, 4, 7, 13, 1, 5, 0, 15, 10, 3, 9, 8, 6],
+         [4, 2, 1, 11, 10, 13, 7, 8, 15, 9, 12, 5, 6, 3, 0, 14],
+         [11, 8, 12, 7, 1, 14, 2, 13, 6, 15, 0, 9, 10, 4, 5, 3 ]
+       ],
+       [ 
+         [12, 1, 10, 15, 9, 2, 6, 8, 0, 13, 3, 4, 14, 7, 5, 11], 
+         [10, 15, 4, 2, 7, 12, 9, 5, 6, 1, 13, 14, 0, 11, 3, 8], 
+         [9, 14, 15, 5, 2, 8, 12, 3, 7, 0, 4, 10, 1, 13, 11, 6],
+         [4, 3, 2, 12, 9, 5, 15, 10, 11, 14, 1, 7, 6, 0, 8, 13 ]
+       ], 
+       [ 
+         [4, 11, 2, 14, 15, 0, 8, 13, 3, 12, 9, 7, 5, 10, 6, 1],
+         [13, 0, 11, 7, 4, 9, 1, 10, 14, 3, 5, 12, 2, 15, 8, 6],
+         [1, 4, 11, 13, 12, 3, 7, 14, 10, 15, 6, 8, 0, 5, 9, 2],
+         [6, 11, 13, 8, 1, 4, 10, 7, 9, 5, 0, 15, 14, 2, 3, 12 ]
+       ],
+       [ 
+         [13, 2, 8, 4, 6, 15, 11, 1, 10, 9, 3, 14, 5, 0, 12, 7], 
+         [1, 15, 13, 8, 10, 3, 7, 4, 12, 5, 6, 11, 0, 14, 9, 2],
+         [7, 11, 4, 1, 9, 12, 14, 2, 0, 6, 10, 13, 15, 3, 5, 8],
+         [2, 1, 14, 7, 4, 10, 8, 13, 15, 12, 9, 0, 3, 5, 6, 11] 
+       ]
+   ]    
+   
+    perm = generate_permutation(block_size)
+    inv_perm = generate_inv_permutation(block_size, perm)
+    PC1 = generate_pc1_permutation(block_size)
+    PC2 = generate_pc2_permutation(len(PC1))
+    exp_perm = generate_expansion_permutation(len(PC2), half)
+    half_perm = generate_half_permutation(half)
+
+    enc_msg = [""]*num_rounds
+    for i in range(0, len(bin_msg), block_size):
+        rounds_msg = encrypt(bin_msg[i:i+block_size], perm, inv_perm, exp_perm, key, PC1, PC2, half_perm, s_boxes, num_rounds)
+        for i in range(num_rounds):
+            enc_msg[i] += rounds_msg[i]
+    
+    return enc_msg
+
+def bin_hex(msg):
+    hex = []
+    for i in range(0, len(msg), 8):
+        ascii_val = int(msg[i:i+8], 2)
+        hex.append("%x"%ascii_val)
+    return "".join(hex)
+
+def init(msg, block_size, num_rounds, key):
+    print(msg, block_size, num_rounds)
+    half = block_size//2
+    enc_msg = preprocess(msg, half, block_size, num_rounds, key)
+    # for i in range(num_rounds):
+    #     enc_msg[i] = bin_hex(enc_msg[i])
+    return enc_msg
+
+if __name__ == "__main__":
+    half = 32
+    block_size = half*2
+    num_rounds = 16
+    key = "".rjust(block_size, '0')
+    enc_msg = preprocess("[%Ú:A", half, block_size, num_rounds, key)
+    print(enc_msg)
+    print(bin_txt(enc_msg))
+    # one_bit_diff("aman")
---
+++ gui.py
@@ -0,0 +1,166 @@
+from tkinter import *
+from tkinter.ttk import *
+from des import init, one_bit_diff, preprocess_key, Weak_key_demonstration
+from matplotlib.figure import Figure
+
+from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg, 
+NavigationToolbar2Tk)
+
+class Combo:
+    def __init__(self, root, values, col):
+        self.combo = Combobox(root)
+        self.combo['values'] = values
+        self.combo.current(0)
+        self.combo.place(x=col, y=20)
+        self.combo.bind("<<ComboboxSelected>>", do_enc)
+    
+    def get(self):
+        return self.combo.get()
+
+def do_enc(event):
+    try:
+        if not len(dec.get()) or not len(user_key.get()):
+            enc_var.set("")
+            return
+        key = preprocess_key(hex_bin(user_key.get()), int(block_size.get())//2)
+        enc_msg = init(dec.get(), int(block_size.get()), int(num_rounds.get()), key)
+        enc_var.set(enc_msg[int(num_rounds.get())-1])
+    
+    except:
+        enc_var.set("")
+
+def draw_graph(diff, labels, label):
+    root1 = Toplevel()
+    L = Label(root1, text=label)
+    L.pack()
+    canvas1 = Canvas(root1, height=HEIGHT, width=WIDTH)
+    canvas1.pack()
+    frame1 = Frame(root1)
+    frame1.place(relx=0.05,rely=0.05,relwidth =0.9, relheight = 0.9,anchor = 'nw')
+    
+    x, y = [], []
+    for z in range(len(diff)):
+        y.append(diff[z])
+        x.append(z)
+
+    fig = Figure(figsize = (6, 6),dpi = 100)                
+    plot1 = fig.add_subplot(111)
+    plot1.set_xlabel(labels[0])
+    plot1.set_ylabel(labels[1])
+    plot1.plot(x,y)
+
+    canvas = FigureCanvasTkAgg(fig, master = frame1)                       
+    canvas.draw()
+    canvas.get_tk_widget().place(relwidth = 1,relheight = 1,anchor = 'nw')
+
+
+def Avalanche_effect_with_ciphertext():
+    try:
+        if not len(dec.get()) or not len(user_key.get()):
+            enc_var.set("")
+            return
+        key = preprocess_key(hex_bin(user_key.get()), int(block_size.get())//2)
+        enc_msg1 = init(dec.get(), int(block_size.get()), int(num_rounds.get()), key)
+        enc_msg2 = init(one_bit_diff(dec.get()), int(block_size.get()), int(num_rounds.get()), key)
+        diff = [0]*(int(num_rounds.get())+1)
+        diff[0] = 1
+        
+        for i in range(1, len(diff)):
+            for j in range(len(enc_msg1[i-1])):
+                diff[i] += (enc_msg1[i-1][j] != enc_msg2[i-1][j])
+        draw_graph(diff, ["x-num rounds", "y-hamming distance betweem two cipher text"], "Avalanche effect wiht cyphertext")
+    except:
+        enc_var.set("")
+
+def hex_bin(msg):
+    return bin(int(msg, 16))[2:]
+
+def Avalanche_effect_with_key():
+    try:
+        if not len(dec.get()) or not len(user_key.get()):
+            enc_var.set("")
+            return
+        key = preprocess_key(hex_bin(user_key.get()), int(block_size.get())//2)
+        enc_msg1 = init(dec.get(), int(block_size.get()), int(num_rounds.get()), key)
+        key1 = key
+        key = preprocess_key(key, int(block_size.get())//2, 1)
+        print(key, key1)
+        enc_msg2 = init(dec.get(), int(block_size.get()), int(num_rounds.get()), key)
+        diff = [0]*(int(num_rounds.get())+1)
+        diff[0] = 1
+        
+        for i in range(1, len(diff)):
+            for j in range(len(enc_msg1[i-1])):
+                diff[i] += (enc_msg1[i-1][j] != enc_msg2[i-1][j])
+        draw_graph(diff, ["x-num rounds", "y-hamming distance betweem two cipher text"], "Avalanche effect with key")
+    except:
+        enc_var.set("")
+
+class userEntry:
+    def __init__(self, root, msg, x, f=0):
+        self.var = StringVar()
+        self.entry = Entry(root, textvariable=self.var)
+        self.entry.insert(0, msg)
+        self.entry.place(relx = x,rely = 0.4, relwidth = 0.4,relheight = 0.2,anchor = 'nw')
+        if f:
+            self.entry.bind("<KeyRelease>", do_enc)
+
+    def get(self):
+        return self.var.get()
+
+def Weak_key():
+    key = preprocess_key(hex_bin(user_key.get()), int(block_size.get())//2)
+    keys = Weak_key_demonstration(key, int(num_rounds.get()), int(block_size.get()))
+
+    root1 = Toplevel()
+    root1.geometry(f"{WIDTH}x{HEIGHT}")
+    fig = Figure(figsize = (6, 6),dpi = 100)      
+    frame1 = Frame(root1)
+    frame1.place(relx=0.05,rely=0.05,relwidth =0.9, relheight = 0.9,anchor = 'nw')
+    var = StringVar()
+    msg = Message(frame1, textvariable=var, width=500)
+    msg.pack()
+    print(keys)
+    tmp = ""
+    for i in range(len(keys)):
+        tmp += "The key for round "+str(i+1)+" is:  "+keys[i]
+        tmp += '\n'
+    var.set(tmp)
+
+if __name__ == "__main__":
+    HEIGHT, WIDTH = 500, 800
+    root = Tk()
+    root.geometry("800x500")
+    root.title('DES encryption and decryption')
+
+    dec_frame = Frame(root, height=200, borderwidth=6, relief=SUNKEN)
+    dec_frame.pack(fill="x")
+    dec_frame.pack_propagate(0)
+
+    block_size = Combo(dec_frame, ("Full Block size", 32, 64, 128), 100)
+    num_rounds = Combo(dec_frame, ("Number of rounds", 1, 8, 16, 32), 500)
+
+    dec = userEntry(dec_frame, "Enter plain text here", 0.55, 1)
+    user_key = userEntry(dec_frame, "Enter key or weak key in hexadecimal form", 0.05)
+    
+    enc_frame = Frame(root, height=200, borderwidth=6, relief=SUNKEN)
+    enc_frame.pack(fill="x")
+    enc_frame.pack_propagate(0)
+
+    enc_label = Label(enc_frame, text="Here Encrypted message will be shown")
+    enc_label.pack(pady=20)
+
+    enc_var = StringVar()
+    enc_msg = Message(enc_frame, textvariable=enc_var, width=500)
+    enc_msg.pack()
+
+    button1 = Button(enc_frame,text = "Avalanche effect(plaintext)", command = Avalanche_effect_with_ciphertext)
+    button1.place(relx = 0.1,rely = 0.7,relwidth = 0.3,relheight = 0.2,anchor = 'nw')
+
+    button2 = Button(enc_frame,text = "Avalanche effect(key)", command = Avalanche_effect_with_key)
+    button2.place(relx = 0.45,rely = 0.7,relwidth = 0.2,relheight = 0.2,anchor = 'nw')
+
+    button3 = Button(enc_frame,text = "Weak key Demonstration", command = Weak_key)
+    button3.place(relx = 0.7,rely = 0.7,relwidth = 0.2,relheight = 0.2,anchor = 'nw')
+
+    root.mainloop()
